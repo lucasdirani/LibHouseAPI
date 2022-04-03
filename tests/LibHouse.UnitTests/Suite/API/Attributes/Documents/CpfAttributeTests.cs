4fa21804de2160aa89b4fc5a31076691f0982417
Lucas Dirani <lruizd@protonmail.com>
2022-04-03 11:13:11 -0300

    Criação de testes unitários para a classe CpfAttribute

---
+++ tests/LibHouse.UnitTests/Suite/API/Attributes/Documents/CpfAttributeTests.cs
@@ -0,0 +1,37 @@
+﻿using LibHouse.API.Attributes.Documents;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.API.Attributes.Documents
+{
+    public class CpfAttributeTests
+    {
+        [Theory]
+        [InlineData("22527856080")]
+        [InlineData("44271974005")]
+        [InlineData("10326783059")]
+        [InlineData("19141144007")]
+        [InlineData("37064090031")]
+        public void IsValid_ValidCpf_ReturnsSuccess(string cpf)
+        {
+            CpfAttribute cpfAttribute = new();
+
+            bool isValid = cpfAttribute.IsValid(cpf);
+
+            Assert.True(isValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("06633959094")]
+        [InlineData("45550841029")]
+        [InlineData("0488990904")]
+        public void IsValid_InvalidCpf_ReturnsFailure(string invalidCpf)
+        {
+            CpfAttribute cpfAttribute = new();
+
+            bool isValid = cpfAttribute.IsValid(invalidCpf);
+
+            Assert.False(isValid);
+        }
+    }
+}
